bad8b763f047d206b83b4e13dd0b3879eaaeab26
ZeyadYasser <zeyady98@gmail.com>
2020-03-19 14:20:32 +0200

    add anonymization handler skeleton for files image

--- lib/py/anonymize/files_anon_handler.py
+++ lib/py/anonymize/files_anon_handler.py
@@ -1,3 +1,20 @@
 
+
+def reg_handler(entry):
+    return entry
+
+def inet_sk_entry(entry):
+    return entry
+
+file_entry_handlers = {
+    'REG': reg_handler,
+    'INETSK': inet_sk_entry,
+}
+
 def files_anon_handler(image_dict):
+    anon_entries = []
+    for entry in image_dict['entries']:
+        handler = file_entry_handlers[entry['type']]
+        anon_entries.append(handler(entry))
+    image_dict['entries'] = anon_entries
     return image_dict
