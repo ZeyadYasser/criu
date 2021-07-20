a4996ed2d1d016c713f14d4b8c0b8bad4173c4a8
Zeyad Yasser <zeyady98@gmail.com>
2021-07-20 15:45:04 +0200

    criu: add nftables connection locking/unlocking
    This adds nftables based connection locking as an alernative
    for iptables. This avoid the external dependency of
    iptables-restore.
    
    It works by creating a 'connection set', which is a set of
    connection identifying tuples. Rules are added to drop packets that
    match the connection tuples in the set. Locking is now reduced to
    just adding the connection identifying tuple to the set.
    
    Unlocking is just as simple as deleteing the CRIU table.
    
    v2: split ip string conversion into two if conditions
    v3: add better message when CRIU is build without libnftables support
    
    Signed-off-by: Zeyad Yasser <zeyady98@gmail.com>

--- criu/net.c
+++ criu/net.c
@@ -3205,8 +3205,11 @@ int network_lock(void)
 	pr_info("Lock network\n");
 
 	/* Each connection will be locked on dump */
-	if  (!(root_ns_mask & CLONE_NEWNET))
+	if  (!(root_ns_mask & CLONE_NEWNET)) {
+		if (opts.network_lock_method == NETWORK_LOCK_NFTABLES)
+			nftables_init_connection_lock();
 		return 0;
+	}
 
 	if (run_scripts(ACT_NET_LOCK))
 		return -1;
@@ -3225,6 +3228,8 @@ void network_unlock(void)
 		/* coverity[check_return] */
 		run_scripts(ACT_NET_UNLOCK);
 		network_unlock_internal();
+	} else if (opts.network_lock_method == NETWORK_LOCK_NFTABLES) {
+		nftables_network_unlock();
 	}
 }
 
